d3b198401bc7b548afc7fc361e26e53c08cefc84
zero318 <zero318hackingcorp@gmail.com>
2021-11-21 17:10:37 -0500

    yay commit script

--- patches/ZeroEclExtensions/patch.js
+++ patches/ZeroEclExtensions/patch.js
@@ -3,9 +3,9 @@
 		"nmlgc/base_tsa"
 	],
 	"id": "ZeroEclExtensions",
-	"title": "(06,07,08) Adds new ECL instructions",
 	"servers": [
 		"https://raw.githubusercontent.com/zero318/thcrap_patches/master/patches/ZeroEclExtensions/"
 	],
-	"version": "0.0.0.1",
+	"title": "(06,07,08) Adds new ECL instructions",
+	"version": "0.0.0.1"
 }
