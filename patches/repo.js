689cc594056d64555b3ca4ae61f5273aa2220403
zero318 <zero318hackingcorp@gmail.com>
2022-08-11 05:52:24 -0400

    yay commit script

--- patches/repo.js
+++ patches/repo.js
@@ -2,13 +2,13 @@
 	"contact": "zero318#4476",
 	"id": "zero318",
 	"patches": {
-		"FourOfAKind": "Challenge all four difficulties at once",
-		"NoDat": "(08) Runs the game without even reading the th.dat file.",
-		"RankConfig": "(06-08) Base patch for easier modification of rank tables.",
-		"SpellcardLimit": "(06) Increases the maximum number of available spellcard IDs.",
-		"TH8_ExtendedTrial": "(08) Ports features of the old IN trials to the final version.",
-		"YoloSpeed": "(08) Removes intentional ingame slowdown.",
-		"ZeroEclExtensions": "(06,07,08,18) Adds new ECL instructions"
+		"FourOfAKind": "FourOfAKind(06) Challenge all four difficulties at once",
+		"NoDat": "NoDat(08) Runs the game without even reading the th.dat file.",
+		"RankConfig": "RankConfigWIP(06-08) Base patch for easier modification of rank tables.",
+		"SpellcardLimit": "SpellcardLimit(06) Increases the maximum number of available spellcard IDs.",
+		"TH8_ExtendedTrial": "TH8_ExtendedTrial(08) Ports features of the old IN trials to the final version.",
+		"YoloSpeed": "YoloSpeed(08) Removes intentional ingame slowdown.",
+		"ZeroEclExtensions": "ZeroEclExtensionsWIP(06,07,08,18) Adds new ECL instructions "
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/zero318/thcrap_patches/master/patches/"
