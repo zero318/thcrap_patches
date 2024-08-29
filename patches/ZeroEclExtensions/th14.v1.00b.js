b6a294c28532005d118ce68fdade58a738dea78a
zero318 <zero318hackingcorp@gmail.com>
2024-08-29 00:55:45 -0400

    yay commit script

--- patches/ZeroEclExtensions/th14.v1.00b.js
+++ patches/ZeroEclExtensions/th14.v1.00b.js
@@ -12,7 +12,7 @@
 		"zecl_hins_jump_base_count": { "val": 169 },
 		"zecl_hins_jump_new_count": { "val": 1 },
 		"zecl_hins_case_addr": { "val": 0x429EF4 },
-		"zecl_hins_case_base_count": { "val": 705 },
+		"zecl_hins_case_base_count": { "val": 704 },
 		"zecl_hins_case_new_count": { "val": 1 },
 		// Int Vars
 		"zecl_ivar_jump_addr": { "val": 0x42AF34 },
