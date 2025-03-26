c8f97ec34d6b160935eca2478ca25e428fbf4d06
zero318 <zero318@users.noreply.github.com>
2025-03-26 17:28:15 -0400

    Update repo.js to remove old/broken patches

--- patches/repo.js
+++ patches/repo.js
@@ -2,12 +2,9 @@
 	"contact": "zero318#4476",
 	"id": "zero318",
 	"patches": {
-		"AoCFNetplayZ": "AoCFNetplayZ(155) Patch version of the AoCF netplay plugin",
 		"FourOfAKind": "FourOfAKind(06) Challenge all four difficulties at once",
 		"GrazeDisplay": "GrazeDisplay(08) Applies tints to things that can be grazed",
 		"InfinitePatchyBooks": "InfinitePatchyBooks(06) What if the books never ended?",
-		"NoDat": "NoDat(08) Runs the game without even reading the th.dat file.",
-		"RankConfig": "RankConfigWIP(06-08) Base patch for easier modification of rank tables.",
 		"SpellcardLimit": "SpellcardLimit(06) Increases the maximum number of available spellcard IDs.",
 		"TH8_ExtendedTrial": "TH8_ExtendedTrial(08) Ports features of the old IN trials to the final version.",
 		"YoloSpeed": "YoloSpeed(06-095) Removes gamespeed changes and time stops.",
