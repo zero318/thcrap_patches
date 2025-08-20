77eeee443dac187c07ed41cd34dae4b375759bda
zero318 <zero318hackingcorp@gmail.com>
2025-08-19 20:35:03 -0400

    e

--- patches/repo.js
+++ patches/repo.js
@@ -2,12 +2,12 @@
 	"contact": "zero318#4476",
 	"id": "zero318",
 	"patches": {
-		"FourOfAKind": "FourOfAKind(06) Challenge all four difficulties at once",
-		"GrazeDisplay": "GrazeDisplay(08) Applies tints to things that can be grazed",
-		"InfinitePatchyBooks": "InfinitePatchyBooks(06) What if the books never ended?",
-		"SpellcardLimit": "SpellcardLimit(06) Increases the maximum number of available spellcard IDs.",
-		"TH8_ExtendedTrial": "TH8_ExtendedTrial(08) Ports features of the old IN trials to the final version.",
-		"YoloSpeed": "YoloSpeed(06-095) Removes gamespeed changes and time stops.",
+		"FourOfAKind": "(06) Challenge all four difficulties at once",
+		"GrazeDisplay": "(08) Applies tints to things that can be grazed",
+		"InfinitePatchyBooks": "(06) What if the books never ended?",
+		"SpellcardLimit": "(06) Increases the maximum number of available spellcard IDs.",
+		"TH8_ExtendedTrial": "(08) Ports features of the old IN trials to the final version.",
+		"YoloSpeed": "(06-095) Removes gamespeed changes and time stops.",
 		"ZeroEclExtensions": "Adds new ECL instructions"
 	},
 	"servers": [
